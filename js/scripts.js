94f0bb576e9487e4473ceed78146c2d9cf155447
Lovenofill <lovenofill@gmail.com>
2021-08-16 15:06:03 +0700

    0.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Coming Soon v6.0.4 (https://startbootstrap.com/theme/coming-soon)
+* Start Bootstrap - Full Width Pics v5.0.3 (https://startbootstrap.com/template/full-width-pics)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
